b73352b8a294300f60b41814f2aa8178cd92ef81
Jhonatan Mejia <quenandres@gmail.com>
2021-08-21 16:39:32 -0500

    Operadores: Asignación, Comparación y Aritméticos.11/22

---
+++ operadores.js
@@ -0,0 +1,55 @@
+// OPERADORES
+
+// ARITMETICOS
+
++ // Suma a + b,  afirmación unitaria +a : Positivo, concatenación "a" + "la" : "ala"
+- // Resta a - b, negación unitaria -a : Negativo
+* // Producto a * b
+/ // Divisor a / b
+% // Residuo a % b
+** // Potencia a ** b, a elevado a b
+++ // Incremento (suma uno) a++ : a = a + 1
+-- // Decremento (resta uno) a-- : a = a - 1
+
+// ASIGNACIÓN
+
+= // Asignación a = b
++= // Asignación de adición  a += b : a = a + b
+-= // Asignación de sustracción  a -= b : a = a - b
+*= // Asignación de multiplicación  a *= b : a = a * b
+/= // Asignación de división  a /= b : a = a / b
+%= // Asignación de residuo  a %= b : a = a % b
+**= // Asignación de potencia  a **= b : a = a ** b
+<<= // Asignación de desplazamiento a la izquierda  a <<= b : a = a << b
+>>= // Asignación de desplazamiento a la derecha  a >>= b : a = a >> b
+>>>= // Asignación sin signo de desplazamiento a la derecha  a >>>= b : a = a >>> b
+&= // Asignación AND  a &= b : a = a & b
+^= // Asignación XOR  a ^= b : a = a ^ b
+|= // Asignación OR  a |= b : a = a | b
+
+// COMPARACIÓN
+
+== // Igualdad a == b, a tiene el mismo valor que b
+!= // Distinto a != b, a tiene un valor diferente a b
+=== // Identidad a === b igual valor igual tipo de dato
+!== // Sin Identidad a !== b igual valor o igual tipo de dato
+> // Mayor que a > b
+>= // Mayor o igual que a >= b
+< // Menor que a < b
+<= // Menor o igual que a <= b
+
+// LOGICOS
+
+&& // AND, Y, a && b : a y b
+|| // OR, O, a || b : a o b
+! // NOT, Negación, a = true : !a = false
+
+// LOGICOS A NIVEL DE BITS
+
+<< // Desplazamiento a la izquierda  a << b
+>> // Desplazamiento a la derecha  a >> b
+>>> // Desplazamiento a la derecha sin signo  a >>> b
+& // AND  a & b
+^ // XOR  a ^ b
+| // OR  a | b
+~ // NOT  a | b```
