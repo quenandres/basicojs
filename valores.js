eb77b8598d83a90867d0be3f93b62b0491b2a2d8
Jhonatan Mejia <quenandres@gmail.com>
2021-08-21 10:47:56 -0500

    Elementos de un Lenguaje de Programación: Variables, Funciones y Sintaxis3/22

---
+++ valores.js
@@ -0,0 +1,18 @@
+40
+
+// Tipos de datos
+/**
+ * Primitivos
+ * 
+ * Entero 0
+ * String "Valor"
+ * Boolean true | false
+ * Valores placeholder null | undefined
+ * 
+ * Objetos
+ * Array [1,2,3]
+ * Objeto { nombre: "Jhonatan"}
+ * 
+ * typeof para verificar el tipo de la variable
+ */
+
