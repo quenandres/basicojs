db572aa0a9867de66f09de321a0f54eb50989f07
Jhonatan Mejia <quenandres@gmail.com>
2021-08-21 15:08:43 -0500

    Hoisting8/22

---
+++ hoisting.js
@@ -0,0 +1,6 @@
+// Hoisting antes de ecmascript 5
+console.log(miNombre);
+var miNombre = "Jhonatan";
+
+
+// JS toma las declaraciones de las variables y las pone en el top de la ejecución, una vez se llama la variable se ejecuta pero con el valor de undefined
