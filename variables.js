5c39019c68c742fee5e8bfaf10e120e44bb2dbc7
Jhonatan Mejia <quenandres@gmail.com>
2021-08-21 10:58:13 -0500

    Variables en JavaScript4/22

---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Jhonatan"; // var palabra reservada de js para que el navegador asigne un lugar en memoria
+
+// Declarar
+var edad; // Reservamos un espacio sin valor.
+
+// Inicializamos
+edad = 30;
+
+// Arrays
+var elementos = ["computadora", "Celular"];
+
+
+var persona = {
+    nombre: "Jhonatan",
+    edad: 26
+}
+
+persona;
