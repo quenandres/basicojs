b819a711589037f9bffd5b613980321108136ab9
Jhonatan Mejia <quenandres@gmail.com>
2021-08-21 15:39:41 -0500

    Coerción9/22

---
+++ coercion.js
@@ -0,0 +1,16 @@
+/*
+Coerción es la forma en la que podemos cambiar un tipo de valor a otro, existen dos tipos de coerción:
+Coerción implícita = es cuando el lenguaje nos ayuda a cambiar el tipo de valor.
+Coerción explicita = es cuando obligamos a que cambie el tipo de valor.
+
+ParseInt vs Number
+
+ParseInt: Analiza la cadena desde el primer dígito, hasta que encuentre algo que no sea dígito y devuelve lo que haya analizado.
+Ejemplo:
+parseInt("123hui") //123
+Number: Busca convertir toda la cadena en un número, por lo que al encontrarse con un elemento que no sea diginto nos dara como resultado NAN.
+Ejemplo:
+Number("123hui") //NaN
+
+Cada día se aprende algo nuevo, no pierdas esa oportunidad y no pares de aprender.
+*/
