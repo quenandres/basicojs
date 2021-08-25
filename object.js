a27f22ec42ee4a81ddf4194d78e024ba26683cbf
Jhonatan Mejia <quenandres@gmail.com>
2021-08-25 00:06:47 -0500

    Objects17/22

---
+++ object.js
@@ -0,0 +1,8 @@
+var miAuto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    anio: 2020,
+    detalleDelAuto: function(){ // Funcion a la que se 
+        console.log(`Auto ${this.modelo} ${this.anio}`)
+    }
+}
